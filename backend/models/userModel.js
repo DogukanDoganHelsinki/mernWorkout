2af18bb5405900ea422398d03777900af601dbc6
Dogukan Dogan <103068134+DogukanDoganHelsinki@users.noreply.github.com>
2024-04-14 22:02:52 +0300

    auth backend almost done

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -16,5 +18,55 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// static signup method
+userSchema.statics.signup = async function (email, password) {
+  // validate email
+  if (!email || !password) {
+    throw new Error("Please provide an email and a password");
+  }
+  if (!validator.isEmail(email)) {
+    throw new Error("Please provide a valid email address");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw new Error(
+      "Password must be at least 8 characters long and contain a number"
+    );
+  }
+
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw new Error("User with this email already exists");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw new Error("Please provide an email and a password");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw new Error("Incorrect email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw new Error("Incorrect password");
+  }
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,8 +5,14 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
-          <h1>Workout buddy</h1>
+          <h1>Workout Buddy</h1>
         </Link>
+        <nav>
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </div>
+        </nav>
       </div>
     </header>
   );
